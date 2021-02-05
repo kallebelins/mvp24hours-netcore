57fc3bdebf0e94d364278b87d520f179e6d96600
kallebelins <44676581+kallebelins@users.noreply.github.com>
2021-02-05 12:59:44 -0300

    [ADD] Operation validator.

---
+++ src/Mvp24Hours.Infrastructure/Pipe/Operations/OperationValidator.cs
@@ -0,0 +1,26 @@
+﻿//=====================================================================================
+// Developed by Kallebe Lins (kallebe.santos@outlook.com)
+// Teacher, Architect, Consultant and Project Leader
+// Virtual Card: https://www.linkedin.com/in/kallebelins
+//=====================================================================================
+// Reproduction or sharing is free!
+//=====================================================================================
+using Mvp24Hours.Core.Contract.Infrastructure.Pipe;
+
+namespace Mvp24Hours.Infrastructure.Pipe.Operations
+{
+    /// <summary>  
+    /// Abstraction of mapping operations
+    /// </summary>
+    public abstract class OperationValidator : OperationBase
+    {
+        public override IPipelineMessage Execute(IPipelineMessage input)
+        {
+            if (!IsValid(input))
+                input.Lock();
+            return input;
+        }
+
+        public abstract bool IsValid(IPipelineMessage input);
+    }
+}
---
+++ src/Mvp24Hours.Infrastructure/Pipe/Operations/OperationValidatorAsync.cs
@@ -0,0 +1,27 @@
+﻿//=====================================================================================
+// Developed by Kallebe Lins (kallebe.santos@outlook.com)
+// Teacher, Architect, Consultant and Project Leader
+// Virtual Card: https://www.linkedin.com/in/kallebelins
+//=====================================================================================
+// Reproduction or sharing is free!
+//=====================================================================================
+using Mvp24Hours.Core.Contract.Infrastructure.Pipe;
+using System.Threading.Tasks;
+
+namespace Mvp24Hours.Infrastructure.Pipe.Operations
+{
+    /// <summary>  
+    /// Abstraction of mapping operations
+    /// </summary>
+    public abstract class OperationValidatorAsync : OperationBaseAsync
+    {
+        public override async Task<IPipelineMessage> Execute(IPipelineMessage input)
+        {
+            if (!await IsValid(input))
+                input.Lock();
+            return input;
+        }
+
+        public abstract Task<bool> IsValid(IPipelineMessage input);
+    }
+}
