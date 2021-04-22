5455eac5339e2ef475abc9b24bee90f36082948e
kallebelins <44676581+kallebelins@users.noreply.github.com>
2021-04-22 16:42:50 -0300

    [UPDATE] Message Quota WCF.

--- src/Mvp24Hours.Infrastructure/Helpers/ServiceRequestHelper.cs
+++ src/Mvp24Hours.Infrastructure/Helpers/ServiceRequestHelper.cs
@@ -9,16 +9,57 @@ namespace Mvp24Hours.Infrastructure.Helpers
         public static TClient Client<TClient>(string url)
             where TClient : class
         {
+            if (url.StartsWith("https"))
+            {
+                return ClientHttps<TClient>(url);
+            }
+            else
+            {
+                return ClientHttp<TClient>(url);
+            }
+        }
+
+        public static TClient ClientHttps<TClient>(string url)
+            where TClient : class
+        {
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
             var binding = new BasicHttpsBinding(BasicHttpsSecurityMode.Transport);
 
             binding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Windows;
-            binding.MaxReceivedMessageSize = Int32.MaxValue;
-            binding.MaxBufferSize = Int32.MaxValue;
+            binding.MaxReceivedMessageSize = int.MaxValue;
+            binding.MaxBufferSize = int.MaxValue;
             binding.ReaderQuotas = System.Xml.XmlDictionaryReaderQuotas.Max;
+            binding.MaxBufferPoolSize = int.MaxValue;
+            binding.MaxBufferSize = int.MaxValue;
+            binding.MaxReceivedMessageSize = int.MaxValue;
+            binding.ReaderQuotas.MaxStringContentLength = int.MaxValue;
+            binding.ReaderQuotas.MaxArrayLength = int.MaxValue;
+            binding.ReaderQuotas.MaxDepth = int.MaxValue;
+            binding.ReaderQuotas.MaxBytesPerRead = int.MaxValue;
 
-            // 229d9aa00eda75238409390d0dedc62367f7d8d1
+            var endpoint = new EndpointAddress(new Uri(url));
+            return (TClient)Activator.CreateInstance(typeof(TClient), binding, endpoint);
+        }
+
+        public static TClient ClientHttp<TClient>(string url)
+            where TClient : class
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            var binding = new BasicHttpBinding(BasicHttpSecurityMode.Transport);
+
+            binding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Windows;
+            binding.MaxReceivedMessageSize = int.MaxValue;
+            binding.MaxBufferSize = int.MaxValue;
+            binding.ReaderQuotas = System.Xml.XmlDictionaryReaderQuotas.Max;
+            binding.MaxBufferPoolSize = int.MaxValue;
+            binding.MaxBufferSize = int.MaxValue;
+            binding.MaxReceivedMessageSize = int.MaxValue;
+            binding.ReaderQuotas.MaxStringContentLength = int.MaxValue;
+            binding.ReaderQuotas.MaxArrayLength = int.MaxValue;
+            binding.ReaderQuotas.MaxDepth = int.MaxValue;
+            binding.ReaderQuotas.MaxBytesPerRead = int.MaxValue;
 
             var endpoint = new EndpointAddress(new Uri(url));
             return (TClient)Activator.CreateInstance(typeof(TClient), binding, endpoint);
