130b2fb23d64dec5f7e5fc5b5140743d1f2b96aa
kallebelins <44676581+kallebelins@users.noreply.github.com>
2021-11-11 09:09:39 -0300

    [REFACTOR] Project restructuring for pipeline separation.

---
+++ src/Mvp24Hours.Infrastructure.Pipe/Extensions/PipelineServiceExtensions.cs
@@ -0,0 +1,39 @@
+﻿//=====================================================================================
+// Developed by Kallebe Lins (kallebe.santos@outlook.com)
+// Teacher, Architect, Consultant and Project Leader
+// Virtual Card: https://www.linkedin.com/in/kallebelins
+//=====================================================================================
+// Reproduction or sharing is free! Contribute to a better world!
+//=====================================================================================
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Mvp24Hours.Core.Contract.Infrastructure.Pipe;
+using Mvp24Hours.Infrastructure.Helpers;
+using Mvp24Hours.Infrastructure.Pipe;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Mvp24Hours.Infrastructure.Extensions
+{
+    public static class PipelineServiceExtensions
+    {
+        /// <summary>
+        /// Add pipeline engine
+        /// </summary>
+        public static IServiceCollection AddMvp24HoursPipeline(this IServiceCollection services, string token = null, bool isBreakOnFail = false)
+        {
+            services.AddScoped<IPipeline>(x => new Pipeline(token, isBreakOnFail));
+            return services;
+        }
+
+        /// <summary>
+        /// Add pipeline engine async
+        /// </summary>
+        public static IServiceCollection AddMvp24HoursPipelineAsync(this IServiceCollection services, string token = null, bool isBreakOnFail = false)
+        {
+            services.AddScoped<IPipelineAsync>(x => new PipelineAsync(token, isBreakOnFail));
+            return services;
+        }
+    }
+}
